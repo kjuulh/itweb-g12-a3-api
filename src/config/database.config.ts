d4573e391e002279acf7ee1827f0504395161d7d
hermansen <hermansendev@gmail.com>
2019-11-26 15:46:17 +0100

    update db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,20 +1,33 @@
-import * as express from 'express'
-import * as mongoose from 'mongoose'
-import * as path from 'path'
-import config from './config'
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as path from 'path';
+import config from './config';
 
 export default (app: express.Express) => {
   for (const model of config.globFiles(config.models)) {
-    require(path.resolve(model))
+    require(path.resolve(model));
+  }
+
+  let dbName: string;
+
+  switch (process.env.ENVIRONMENT) {
+    case 'staging':
+      dbName = 'itweb-g12-a3-staging';
+      break;
+    case 'production':
+    case 'development':
+    default:
+      dbName = 'itweb-g12-a3';
+      break;
   }
 
   if (config.useMongo) {
     mongoose
       .connect(config.mongodb, {
-        dbName: 'itweb-g12-a2',
+        dbName,
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
-      .catch(err => console.log('Error connecting to mongo' + err))
+      .catch((err) => console.log('Error connecting to mongo' + err));
   }
-}
+};
